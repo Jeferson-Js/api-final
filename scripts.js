b6f49f93177f692bcc205e4044f8fb5eeaab138a
Jeferson Santos Oliveira <Jeferson Santos Oliveira>
2022-11-13 20:50:26 -0300

    first commit

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const nameUser = document.querySelector('name');
 
 const fetchNames = async (names) => {
-    const response = await fetch("https://api-names.netlify.app/api.json");
+const response = await fetch(`https://api-names.netlify.app/${name}`);
     const data = response.json();
 
     return data;
